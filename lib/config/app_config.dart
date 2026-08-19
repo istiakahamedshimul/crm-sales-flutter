47a3cd5dfb95b07d12c3b92162ffb101e7389c60
Istiak Ahmed <22312104@iubat.edu>
2026-08-19 21:48:00 +0600

    Point sales app to new CRM server

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   const AppConfig._();
 
-  static const apiBaseUrl = 'http://20.219.186.208:5000/api';
+  static const apiBaseUrl = 'http://20.219.131.147:5000/api';
   static const defaultEmail = 'sales@crm.local';
   static const defaultPassword = 'Sales@12345';
 }
